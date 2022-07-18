c0e1f4abbc9950b5c7ce5fccb161fc8d208b1a60
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-18 16:02:39 +0500

    altering MONGO DB project to basic Mongoose

---
+++ app.js
@@ -0,0 +1,89 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitDB';
+
+// Create a new MongoClient
+const client = new MongoClient(url,{useNewUrlParser:true});
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  // insertDocuments(db,()=>{
+  //   client.close();
+  // })
+
+  findDocuments(db,()=>{
+    client.close();
+  })
+
+  // findOneDocuments(db,()=>{
+  //   client.close();
+  // })
+});
+
+// insert document
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name:"Orange",
+      score: 12,
+      review: "sour fruit"
+    },
+    {
+      name:"guava",
+      score: 13,
+      review: "tasty"
+    },
+    {
+      name:"Pomegranate",
+      score: 14,
+      review: "red fruit"
+    }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.insertedCount);
+    assert.equal(3, Object.keys(result.insertedIds).length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+
+//find all documents
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
+  });
+}
+
+//find with Query 
+const findOneDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({name: 'Banana', score:6}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits);
+    callback(fruits);
+  });
+}
